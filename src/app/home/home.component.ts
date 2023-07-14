60a4d13e276129178c3e519f89197cceb1ce8eaa
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-07-13 20:29:43 -0500

    se añade GSAP libreria y animaciones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements AfterViewInit {
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  title = 'greensock';
 
+  @ViewChild('box', { static: true })
+  box!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    gsap.registerEffect({
+      name: "fade",
+      effect: (targets: any, config: any) => {
+        return gsap.to(targets, { duration: config.duration, opacity: 0 });
+      },
+      defaults: { duration: 8 },
+      extendTimeline: true,
+    });
+
+    gsap.from(".box", { duration: 5, opacity: 0, ease: "slow(2, 1.5, true)", y: -800 });
+    gsap.effects['fade'](".subtitle");
+  }
 }
