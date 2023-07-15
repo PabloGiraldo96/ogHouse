bffc0828fe148c7fa253d487ce5a4b4fc9ef10bd
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-07-14 20:53:07 -0500

    se añade animacion gsap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,10 +24,13 @@ export class HomeComponent implements AfterViewInit {
       extendTimeline: true,
     });
 
-    gsap.from(".box", { duration: 3, opacity: 0, ease: "slow(2, 1.5, true)", y: -400 });
-    gsap.effects['fade'](".subtitle");
+    gsap.to('.subtitle', { opacity: 0, duration: 12, repeat: 1, yoyo: true });
+    //gsap.to('.picture', { opacity: 1, duration: 4.5, repeat: 0, yoyo: true });
 
-    gsap.from(".picture", { opacity: 0, duration: 15, ease: "slow(2, 1.5, true)"}); 
+    gsap.from(".box", { duration: 5, opacity: 0, ease: "slow(2, 1.5, true)", y: -400 });
+    //gsap.effects['fade']("");
+
+    gsap.from(".picture", { opacity: 0, duration: 7, ease: "ease(1, 1.5, true)", y: 450, x: 500}); 
 
   }
 }
